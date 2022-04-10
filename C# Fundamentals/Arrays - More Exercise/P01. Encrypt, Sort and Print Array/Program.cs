227eae2e415ad087352f0ee1bfa0b1e23965a4f1
Nikolay Dobrev <nickndobrev@gmail.com>
2022-04-10 16:23:29 +0300

    Arrays-moreEx

---
+++ C# Fundamentals/Arrays - More Exercise/P01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Arrays___More_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputLines = int.Parse(Console.ReadLine());
+            int[] sums = new int[inputLines];
+            string input = string.Empty;
+            for(int i=0;i<inputLines; i++)
+            {
+                int vowelSum = 0;
+                int consonantSum = 0;
+                foreach(char letter in (input=Console.ReadLine()))
+                {
+                    bool isVowel = "aeiouAEIOU".IndexOf(letter)>=0;
+                    if (isVowel)
+                    {
+                        vowelSum += letter * input.Length;
+                    }
+                    else
+                    {
+                        consonantSum += letter / input.Length;
+                    }
+                }
+                sums[i] = vowelSum + consonantSum;
+            }
+            Array.Sort(sums);
+            foreach(int sum in sums)
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
