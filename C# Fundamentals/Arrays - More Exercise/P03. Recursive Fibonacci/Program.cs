25662270b2d39586473834a332f1b83dbd937740
Nikolay Dobrev <nickndobrev@gmail.com>
2022-04-12 21:31:46 +0300

    Arrays - More Exercise full

---
+++ C# Fundamentals/Arrays - More Exercise/P03. Recursive Fibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P03._Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fibNum = int.Parse(Console.ReadLine());
+            int[] fibArr = new int[fibNum+2];
+            fibArr[0] = 1;
+            fibArr[1] = 1;
+            for(int i = 2; i < fibArr.Length && fibNum > 2; i++)
+            {
+                fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+            }
+            Console.WriteLine(fibArr[fibNum-1]);
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays - More Exercise/P04. Fold and Sum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace P04._Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] upperRowBigin = new int[numArr.Length / 4];
+            int[] upperRowEnd = new int[numArr.Length / 4];
+            int[] lowerRow = new int[numArr.Length / 2];
+            int[] upperRow = new int[numArr.Length / 2];
+            int posUpperRowBegin = 0;
+            int posUpperRowEnd = 0;
+            int posLowerRow = 0;
+            for(int i = 0; i < numArr.Length; i++)
+            {
+                if (i < numArr.Length / 4)
+                {
+                    upperRowBigin[posUpperRowBegin++]=numArr[i];
+                }
+                else if (i >= numArr.Length - numArr.Length / 4)
+                {
+                    upperRowEnd[posUpperRowEnd++] = numArr[i];
+                }
+                else
+                {
+                    lowerRow[posLowerRow++] = numArr[i];
+                }
+            }
+            Array.Reverse(upperRowEnd);
+            Array.Reverse(upperRowBigin);
+            for(int i = 0; i < upperRow.Length; i++)
+            {
+                if (i < upperRowBigin.Length)
+                {
+                    upperRow[i] = upperRowBigin[i];
+                }
+                else
+                {
+                    upperRow[i] = upperRowEnd[i - upperRowEnd.Length];
+                }
+            }
+            int[] sum = new int[numArr.Length / 2];
+            for(int i = 0; i < lowerRow.Length; i++)
+            {
+                sum[i] = lowerRow[i] + upperRow[i];
+            }
+            Console.WriteLine(string.Join(" ", sum));
+        }
+    }
+}
