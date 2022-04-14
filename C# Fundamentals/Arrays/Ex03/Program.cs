d860b021777c815dbb856dbaf4bb16e1d0e7e804
Nikolay Dobrev <nickndobrev@gmail.com>
2022-04-14 19:45:33 +0300

    arrays, one more time

---
+++ C# Fundamentals/Arrays/Ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbersForRounding = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            int[] roundedNums = new int[numbersForRounding.Length];
+            for(int i = 0; i < roundedNums.Length; i++)
+            {
+                roundedNums[i] = (int)Math.Round(numbersForRounding[i], MidpointRounding.AwayFromZero);
+            }
+
+            for(int i = 0; i < roundedNums.Length; i++)
+            {
+                Console.WriteLine($"{Convert.ToDecimal(numbersForRounding[i])} => {Convert.ToDecimal(roundedNums[i])}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Arrays/Ex04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letters = Console.ReadLine().Split(' ').ToArray();
+            
+            for(int i=0; i < letters.Length / 2; i++)
+            {
+                string currLetter = letters[i];
+                letters[i] = letters[letters.Length - 1 - i];
+                letters[letters.Length - 1 - i] = currLetter;
+            }
+            //string.Join to write the whole array
+            Console.WriteLine(string.Join(" ", letters));
+        }
+    }
+}
