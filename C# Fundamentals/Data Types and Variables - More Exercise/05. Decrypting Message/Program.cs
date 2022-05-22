53b0dd7a3678d3df3755b1ca74e8ccabd621d6c0
Nikolay Dobrev <nickndobrev@gmail.com>
2022-05-22 23:54:31 +0300

    Data Types and Variables - More Exercise 05

---
+++ C# Fundamentals/Data Types and Variables - More Exercise/05. Decrypting Message/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte decriptionInput = byte.Parse(Console.ReadLine());
+            byte Count = byte.Parse(Console.ReadLine());
+            string message = "";
+
+            while (Count > 0)
+            {
+                message += ((char)(char.Parse(Console.ReadLine()) + decriptionInput)).ToString();
+
+                Count--;
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
