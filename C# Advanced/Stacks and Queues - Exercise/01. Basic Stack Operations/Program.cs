5ffcb813ebd3ae37ea3480c6fd744de36890ca6f
Nikolay Dobrev <nickndobrev@gmail.com>
2022-05-22 22:57:47 +0300

    Stacks and Queues - Exercise part 1

---
+++ C# Advanced/Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    class Program
+    {
+        //60/100
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stackNum = new Stack<int>();
+
+            int[] numbersToPush = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+
+            foreach(int num in numbersToPush)
+            {
+                stackNum.Push(num);
+            }
+
+            for(int i = 0; i < numbers[1]; i++)
+            {
+                stackNum.Pop();
+            }
+
+            int min = int.MaxValue;
+
+            while (stackNum.Count > 0)
+            {
+                if (stackNum.Peek() == numbers[2])
+                {
+                    Console.WriteLine("true");
+                    return;
+                }
+                else
+                {
+                    if (min > stackNum.Peek())
+                    {
+                        min = stackNum.Peek();
+                    }
+
+                    stackNum.Pop();
+                }
+            }
+
+            if (stackNum.Count == 0)
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            if (min != int.MaxValue)
+            {
+                Console.WriteLine(min);
+            }
+        }
+    }
+}
