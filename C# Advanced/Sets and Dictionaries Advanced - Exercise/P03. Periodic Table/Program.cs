8f253fcfceb6c90abe10d2918cf29a73b09693da
Nikolay Dobrev <nickndobrev@gmail.com>
2022-05-28 16:03:51 +0300

    Sets and Dictionaries Advanced - Exercise P03

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/P03. Periodic Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03._Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputs = int.Parse(Console.ReadLine());
+
+            SortedSet<string> elements = new SortedSet<string>();
+
+            for(int i = 0; i < inputs; i++)
+            {
+                string[] compounds = Console.ReadLine().Split();
+                foreach(string element in compounds)
+                {
+                    elements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
