791842a6021373d948679972e52169baa8f17c74
Nikolay Dobrev <nickndobrev@gmail.com>
2022-05-29 14:59:40 +0300

    Sets and Dictionaries Advanced - Exercise P05

---
+++ C# Advanced/Sets and Dictionaries Advanced - Exercise/P05. Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P05._Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> numOfCh = new SortedDictionary<char, int>();
+
+            foreach(char ch in text)
+            {
+                if (!numOfCh.ContainsKey(ch))
+                {
+                    numOfCh[ch] = 1;
+                }
+                else
+                {
+                    numOfCh[ch]++;
+                }
+            }
+
+            foreach(KeyValuePair<char,int> kvp in numOfCh)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
