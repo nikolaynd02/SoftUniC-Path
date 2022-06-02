380e87cf1f31eca322057121080521f0f8e3a453
Nikolay Dobrev <nickndobrev@gmail.com>
2022-06-03 00:58:37 +0300

    Stacks and Queues - Exercise/P06. Songs Queue

---
+++ C# Advanced/Stacks and Queues - Exercise/P06. Songs Queue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06._Songs_Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] songsToBeQueued = Console.ReadLine().Split(", ");
+
+            Queue<string> songsQueue = new Queue<string>(songsToBeQueued);
+
+            string[] cmd = { };
+
+            while (songsQueue.Count > 0)
+            {
+                cmd = Console.ReadLine().Split(new[] {' '}, 2);
+
+                if (cmd[0] == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if(cmd[0] == "Add")
+                {
+                    if (songsQueue.Contains(cmd[1]))
+                    {
+                        Console.WriteLine($"{cmd[1]} is already contained!");
+                    }
+                    else
+                    {
+                        songsQueue.Enqueue(cmd[1]);
+                    }
+                }
+                else if(cmd[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
