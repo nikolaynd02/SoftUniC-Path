c0ae27766ef91ca1a5cf35a2492d919c5e1d2b9b
Nikolay Dobrev <nickndobrev@gmail.com>
2022-06-22 22:17:14 +0300

    77/100 to be edited

---
+++ C# Advanced/Exam Preparation/D.13 April 2022 P.01 MealPalen/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace D._13_April_2022_P._01_MealPalen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> meals = new Dictionary<string, int>()
+            {
+                ["salad"] = 350,
+                ["soup"] = 490,
+                ["pasta"] = 680,
+                ["steak"] = 790
+            };
+
+            Queue<string> allowedMeals = new Queue<string>(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries));
+
+            int[] caloriesTarget = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int targetCount = 0;
+
+            int mealsEaten = 0;
+
+            while(allowedMeals.Count != 0 && caloriesTarget.Sum() > 0)
+            {
+                int currCalTarget = caloriesTarget.Length - (1 + targetCount);
+
+                caloriesTarget[currCalTarget] -= meals[allowedMeals.Dequeue()];
+                mealsEaten++;
+
+                if(caloriesTarget[currCalTarget] <= 0 && caloriesTarget.Sum() > 0)
+                {
+                    caloriesTarget[currCalTarget - 1] += caloriesTarget[currCalTarget];
+                    targetCount++;
+                }
+                else
+                {
+
+                }
+            }
+
+            Stack<int> caloriesLeft = new Stack<int>();
+            foreach(int calories in caloriesTarget)
+            {
+                if (calories > 0)
+                {
+                    caloriesLeft.Push(calories);
+                }
+            }
+
+            if(allowedMeals.Count == 0)
+            {
+                Console.WriteLine($"John had {mealsEaten} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", caloriesLeft)} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {mealsEaten} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", allowedMeals)}.");
+            }
+            
+        }
+    }
+}
