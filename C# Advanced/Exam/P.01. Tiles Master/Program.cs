4d10b3028b065886393a476c47cee269927195d4
Nikolay Dobrev <nickndobrev@gmail.com>
2022-06-25 13:34:45 +0300

    exam 100/100 part 1 and 2

---
+++ C# Advanced/Exam/P.01. Tiles Master/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P._01._Tiles_Master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@$"-Renovator: {12}
+--Speciality: {10}
+--Rate per day: {2} BGN");
+
+            Dictionary<int, int> tilesAndLocations = new Dictionary<int, int>()
+            {
+                [40] = 0,
+                [50] = 0,
+                [60] = 0,
+                [70] = 0
+            };
+
+            int floorCount = 0;
+
+            Stack<int> whiteTilesAreas = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+            Queue<int> greyTilesAreas = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
+
+            while (whiteTilesAreas.Count() > 0 && greyTilesAreas.Count() > 0)
+            {
+                int currWhiteTile = whiteTilesAreas.Peek();
+                int currGreyTile = greyTilesAreas.Peek();
+
+                if (currGreyTile == currWhiteTile)
+                {
+                    int sum = currWhiteTile + currGreyTile;
+
+                    if (tilesAndLocations.ContainsKey(sum))
+                    {
+                        tilesAndLocations[sum]++;
+                    }
+                    else
+                    {
+                        floorCount++;
+                    }
+
+                    whiteTilesAreas.Pop();
+                    greyTilesAreas.Dequeue();
+                }
+                else
+                {
+                    whiteTilesAreas.Push(whiteTilesAreas.Pop() / 2);
+                    greyTilesAreas.Enqueue(greyTilesAreas.Dequeue());
+                }
+            }
+
+            if (whiteTilesAreas.Count() == 0)
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ",whiteTilesAreas)}");
+            }
+
+            if(greyTilesAreas.Count() == 0)
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", greyTilesAreas)}");
+            }
+
+            Dictionary<string, int> finalDic = new Dictionary<string, int>()
+            {
+                ["Countertop"] = tilesAndLocations[60],
+                ["Floor"] = floorCount,
+                ["Oven"] = tilesAndLocations[50],
+                ["Sink"] = tilesAndLocations[40],
+                ["Wall"] = tilesAndLocations[70]
+            };
+
+
+            Dictionary<string, int> d = new Dictionary<string, int>(finalDic.OrderByDescending(x => x.Value).ThenBy(y => y.Key));
+
+            foreach(var kvp in d)
+            {
+                if (kvp.Value > 0)
+                {
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/Exam/P.02 Wall Destroyer/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+
+namespace P._02_Wall_Destroyer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wallSize = int.Parse(Console.ReadLine());
+
+            string[,] wall = new string[wallSize, wallSize];
+
+            int[] vankoCordinates = new int[2];
+
+            int maxHoles = 0;
+            int holesCount = 0;
+
+            for(int x = 0; x < wallSize; x++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for(int y=0; y < wallSize; y++)
+                {
+                    wall[x, y] = input[y].ToString();
+
+                    if (input[y] == 'V')
+                    {
+                        vankoCordinates[0] = x;
+                        vankoCordinates[1] = y;
+                        wall[x, y] = "*";
+                        holesCount++;
+                    }
+                    if (input[y] == '-')
+                    {
+                        maxHoles++;
+                    }
+                }
+            }
+
+            int rodsHit = 0;
+
+
+            string cmd = string.Empty;
+            bool dead = false;
+
+            while((cmd = Console.ReadLine())!= "End")
+            {
+                bool wasHole = false;
+                if (cmd == "left")
+                {
+                    vankoCordinates[1]--;
+
+                    if (ValidateCordanates(vankoCordinates, wallSize))
+                    {
+                        if (wall[vankoCordinates[0], vankoCordinates[1]] == "R")
+                        {
+                            rodsHit++;
+                            vankoCordinates[1]++;
+                            Console.WriteLine("Vanko hit a rod!");
+                        }
+                        else if(wall[vankoCordinates[0], vankoCordinates[1]] == "C")
+                        {
+                            wall[vankoCordinates[0], vankoCordinates[1]] = "E";
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++holesCount} hole(s).");
+                            dead = true;
+                            break;
+                        }
+                        else
+                        {
+                            if(wall[vankoCordinates[0], vankoCordinates[1]] == "-")
+                            {
+                                wall[vankoCordinates[0], vankoCordinates[1]] = "*";
+                                holesCount++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"The wall is already destroyed at position [{vankoCordinates[0]}, {vankoCordinates[1]}]!");
+                            }
+                            
+                        }
+                    }
+                    else
+                    {
+                        vankoCordinates[1]++;
+                    }
+                }
+                else if (cmd == "right")
+                {
+                    vankoCordinates[1]++;
+
+                    if (ValidateCordanates(vankoCordinates, wallSize))
+                    {
+                        if (wall[vankoCordinates[0], vankoCordinates[1]] == "R")
+                        {
+                            rodsHit++;
+                            vankoCordinates[1]--;
+                            Console.WriteLine("Vanko hit a rod!");
+                        }
+                        else if (wall[vankoCordinates[0], vankoCordinates[1]] == "C")
+                        {
+                            wall[vankoCordinates[0], vankoCordinates[1]] = "E";
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++holesCount} hole(s).");
+                            dead = true;
+                            break;
+                        }
+                        else
+                        {
+                            if (wall[vankoCordinates[0], vankoCordinates[1]] == "-")
+                            {
+                                wall[vankoCordinates[0], vankoCordinates[1]] = "*";
+                                holesCount++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"The wall is already destroyed at position [{vankoCordinates[0]}, {vankoCordinates[1]}]!");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        vankoCordinates[1]--;
+                    }
+                }
+                else if (cmd == "up")
+                {
+                    vankoCordinates[0]--;
+
+                    if (ValidateCordanates(vankoCordinates, wallSize))
+                    {
+                        if (wall[vankoCordinates[0], vankoCordinates[1]] == "R")
+                        {
+                            rodsHit++;
+                            vankoCordinates[0]++;
+                            Console.WriteLine("Vanko hit a rod!");
+                        }
+                        else if (wall[vankoCordinates[0], vankoCordinates[1]] == "C")
+                        {
+                            wall[vankoCordinates[0], vankoCordinates[1]] = "E";
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++holesCount} hole(s).");
+                            dead = true;
+                            break;
+                        }
+                        else
+                        {
+                            if (wall[vankoCordinates[0], vankoCordinates[1]] == "-")
+                            {
+                                wall[vankoCordinates[0], vankoCordinates[1]] = "*";
+                                holesCount++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"The wall is already destroyed at position [{vankoCordinates[0]}, {vankoCordinates[1]}]!");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        vankoCordinates[0]++;
+                    }
+                }
+                else if (cmd == "down")
+                {
+                    vankoCordinates[0]++;
+
+                    if (ValidateCordanates(vankoCordinates, wallSize))
+                    {
+                        if (wall[vankoCordinates[0], vankoCordinates[1]] == "R")
+                        {
+                            rodsHit++;
+                            vankoCordinates[0]--;
+                            Console.WriteLine("Vanko hit a rod!");
+                        }
+                        else if (wall[vankoCordinates[0], vankoCordinates[1]] == "C")
+                        {
+                            wall[vankoCordinates[0], vankoCordinates[1]] = "E";
+                            Console.WriteLine($"Vanko got electrocuted, but he managed to make {++holesCount} hole(s).");
+                            dead = true;
+                            break;
+                        }
+                        else
+                        {
+                            if (wall[vankoCordinates[0], vankoCordinates[1]] == "-")
+                            {
+                                wall[vankoCordinates[0], vankoCordinates[1]] = "*";
+                                holesCount++;
+                            }
+                            else
+                            {
+                                Console.WriteLine($"The wall is already destroyed at position [{vankoCordinates[0]}, {vankoCordinates[1]}]!");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        vankoCordinates[0]--;
+                    }
+                }
+            }
+
+            if (!dead)
+            {
+                Console.WriteLine($"Vanko managed to make {holesCount} hole(s) and he hit only {rodsHit} rod(s).");
+            }
+
+            for(int x = 0; x < wallSize; x++)
+            {
+                for(int y = 0; y < wallSize; y++)
+                {
+                    if(vankoCordinates[0] == x && vankoCordinates[1] == y && !dead)
+                    {
+                        Console.Write('V');
+                    }
+                    else
+                    {
+                        Console.Write(wall[x,y]);
+                    }
+                }
+                Console.WriteLine();
+            }
+            
+        }
+
+        static bool ValidateCordanates(int[] vanko,int size)
+        {
+            if (vanko[0] >= 0 && vanko[0] < size && vanko[1] >= 0 && vanko[1] < size)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
