54a655d01eebf04ef8ecd71bea705ddd5bd4c60a
Nikolay Dobrev <nickndobrev@gmail.com>
2022-06-29 21:04:43 +0300

    Recursive Factorial

---
+++ C# Algorithms/Recursion and Backtracking - Lab/P04. Recursive Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P04._Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetFactorial(n));
+        }
+
+        private static int GetFactorial(int n)
+        {
+            if(n==1 || n == 0)
+            {
+                return 1;
+            }
+
+            return n * GetFactorial(n - 1);
+        }
+    }
+}
