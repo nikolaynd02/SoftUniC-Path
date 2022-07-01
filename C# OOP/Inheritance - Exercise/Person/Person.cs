e3df2b5fe5cc843f4a983c681d84f3839974a8d7
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-01 20:16:13 +0300

    Inheritance - Exercise/Person

---
+++ C# OOP/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private int age;
+        private string name;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public virtual int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+
+                age = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append(string.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return strBuilder.ToString();
+        }
+    }
+}
