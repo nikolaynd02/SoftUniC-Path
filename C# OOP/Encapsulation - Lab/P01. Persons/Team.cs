8aaebe2ef801a215623a1a96769e304ce854e925
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-02 20:13:02 +0300

    Encapsulation Lab

---
+++ C# OOP/Encapsulation - Lab/P01. Persons/Team.cs
@@ -0,0 +1,54 @@
+﻿using PersonsInfo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01._Persons
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            firstTeam = new List<Person>();
+            reserveTeam = new List<Person>();
+
+        }
+
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get { return this.firstTeam.AsReadOnly(); }
+           
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get { return this.reserveTeam.AsReadOnly(); }
+            
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+    }
+}
