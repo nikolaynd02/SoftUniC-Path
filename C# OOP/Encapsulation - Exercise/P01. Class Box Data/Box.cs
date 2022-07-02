52dac733c2d7ba6213c0c46f0b8d837bef9f773d
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-02 21:36:06 +0300

    first damn problem

---
+++ C# OOP/Encapsulation - Exercise/P01. Class Box Data/Box.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData 
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * (this.width * this.height) + 2 * (this.width * this.length) + 2 * (this.height * this.length);
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * (this.width * this.height) + 2 * (this.height * this.length);
+        }
+
+        public double Volume()
+        {
+            return this.length * this.width * this.height;
+        }
+
+
+    }
+}
---
+++ C# OOP/Encapsulation - Exercise/P01. Class Box Data/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassBoxData 
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            Box box;
+            try
+            {
+                box = new Box(length,width,height);
+            }
+            catch (ArgumentException argument)
+            {
+                Console.WriteLine(argument.Message);
+                return;
+            }
+
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+    }
+}
