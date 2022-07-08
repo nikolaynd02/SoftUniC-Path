c5de5ab2d0c26732f5e949d8fe7bca9c7be4c906
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-08 21:40:06 +0300

    Exceptions and Error Handling - Lab Problem 1

---
+++ C# OOP/Exceptions and Error Handling - Lab/SquareRoot/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                int sqr = (int)Math.Sqrt(num);
+
+                Console.WriteLine(sqr);
+
+            }
+            catch(Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
