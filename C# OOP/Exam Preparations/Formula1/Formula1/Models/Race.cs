0fdceb808ca30f4ba70c288262a7f9f44dcca96a
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-09 22:04:52 +0300

    task 1/3 50/50 points

---
+++ C# OOP/Exam Preparations/Formula1/Formula1/Models/Race.cs
@@ -0,0 +1,73 @@
+﻿using Formula1.Models.Contracts;
+using Formula1.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private readonly ICollection<IPilot> pilots;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            pilots = new List<IPilot>();
+
+        }
+
+        public string RaceName
+        {
+            get { return raceName; }
+            private set
+            {
+                if(string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidRaceName, value));
+                }
+
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get { return numberOfLaps; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidLapNumbers, value));
+                }
+
+                numberOfLaps = value;
+            }
+                
+        }
+
+        public bool TookPlace { get; set; } = false;
+
+        public ICollection<IPilot> Pilots => pilots;
+
+        public void AddPilot(IPilot pilot)
+        {
+            this.pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder text = new StringBuilder();
+
+            text.AppendLine($"The { RaceName } race has:");
+            text.AppendLine($"Participants: { Pilots.Count}");
+            text.AppendLine($"Number of laps: { NumberOfLaps}");
+            text.AppendLine($"Took place: {(TookPlace ? "Yes" : "No")}");
+
+            return text.ToString().Trim();
+        }
+    }
+}
