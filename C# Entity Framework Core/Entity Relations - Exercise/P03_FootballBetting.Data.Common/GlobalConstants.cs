212ffe2dab979d4de6aa2af7e0e7737abd160afd
Nikolay Dobrev <nickndobrev@gmail.com>
2022-07-26 16:56:43 +0300

    Second Exercise

---
+++ C# Entity Framework Core/Entity Relations - Exercise/P03_FootballBetting.Data.Common/GlobalConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P03_FootballBetting.Data.Common
+{
+    public static class GlobalConstants
+    {
+        //Team
+        public const int TeamNameMaxLenght = 100;
+        public const int TeamLogoUrlMaxLenght = 2048;
+        public const int TeamInitialsMaxLenght = 4;
+
+    }
+}
