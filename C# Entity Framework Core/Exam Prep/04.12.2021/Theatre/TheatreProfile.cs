1b79652fe5571b140a5fda7822578c602c17af8f
Nikolay Dobrev <nickndobrev@gmail.com>
2022-08-03 22:55:10 +0300

    for last

---
+++ C# Entity Framework Core/Exam Prep/04.12.2021/Theatre/TheatreProfile.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Theatre
+{
+    using Data.Models;
+    using DataProcessor.ImportDto;
+    using AutoMapper;
+    class TheatreProfile : Profile
+    {
+        // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE THIS CLASS
+        public TheatreProfile()
+        {
+            
+            
+        }
+    }
+}
