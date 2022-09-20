6eff4c609d74c89d1101c69b2fb820241db6d17b
Nikolay Dobrev <nickndobrev@gmail.com>
2022-09-21 00:27:50 +0300

    arrays - finished

--- JS Advanced/Arrays/Arrays - Exercise.js
+++ JS Advanced/Arrays/Arrays - Exercise.js
@@ -41,8 +41,86 @@ function RotateArray(arr, step){
     console.log(arr.join(` `));
 }
 
-console.log(RotateArray(['Banana', 
-'Orange', 
-'Coconut', 
-'Apple'], 
-15));
+
+//P.05
+function IncSubsetArray(arr){
+    let result = [];
+    let biggestNum = 0;
+
+    for(let num of arr){
+        if( biggestNum <= num ){
+            result.push(num);
+            biggestNum = num;
+        }
+    }
+    return result;
+}
+
+//P.06
+function ListNames(arr){
+    let result = arr
+    .sort((a, b) => a.localeCompare(b))
+    .forEach((el, number) => console.log(`${number + 1}.${el}`));
+}
+
+//P.07
+function SortingNumbers(arr){
+    let sorted = arr.sort((a, b) => a - b);
+    let result = [];
+
+    while (sorted.length != 0) {
+        result.push(sorted.shift(), sorted.pop());
+    }
+
+    return result;
+}
+
+//P.08
+function SortArrayWith2Criteria(arr){
+    let result = arr.sort((a, b) => {
+        if(a.length == b.length){
+            return a.localeCompare(b);
+        }
+        else{
+            return a.length - b.length;
+        }
+    });
+    return result.join(`\n`);
+}
+
+//P.09
+function MagicMatrices(arr){
+    let target = arr[0].reduce((a, b) => a + b);
+    let size = arr.length;
+    let rowSum = 0;
+    let isMagic = true;
+
+    arr.forEach(arr => {
+        rowSum = arr.reduce((acc, num) => acc + num);
+        if (rowSum != target) {
+            isMagic = false;
+        }
+    })
+
+    if (!isMagic) {
+        return false;
+    }
+
+    for (let col = 0; col < size; col++) {
+        let colSum = 0;
+        for (let row = 0; row < size; row++) {
+            colSum += arr[row][col];
+        }
+
+        if (colSum != target) {
+            isMagic = false;
+            break;
+        }
+    }
+    
+    return isMagic;
+}
+
+console.log(MagicMatrices([[4, 5, 6],
+    [6, 5, 4],
+    [5, 5, 5]]));
