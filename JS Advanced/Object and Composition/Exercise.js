5ed7488ace9def656fab50586b93cb07ffeb4cbd
Nikolay Dobrev <nickndobrev@gmail.com>
2022-09-27 23:43:10 +0300

    obj and comp

--- JS Advanced/Object and Composition/Exercise.js
+++ JS Advanced/Object and Composition/Exercise.js
@@ -98,11 +98,37 @@ function LowestPriceInCities(input){
     }
 }
 
+//P.06
+function StoreCatalogue(input){
+    let result = {};
 
-LowestPriceInCities(['Sample Town | Sample Product | 1000',
-    'Sample Town | Orange | 2',
-    'Sample Town | Peach | 1',
-    'Sofia | Orange | 3',
-    'Sofia | Peach | 2',
-    'New York | Sample Product | 1000.1',
-    'New York | Burger | 10']);
+    for(const obj of input.sort((a, b) => a.localeCompare(b))){
+        let [product, price] = obj.split(` : `);
+        price = Number(price);
+
+        let firstLetter = product[0];
+        let object = {product, price};
+
+        if(!result[firstLetter]){
+            result[firstLetter] = [];
+        }
+        result[firstLetter].push(object);
+    }
+
+    for(const item of Object.keys(result)){
+        console.log(item);
+        for(const pr of result[item]){
+            console.log(`  ${pr.product}: ${pr.price}`);
+        }
+    }
+}
+
+StoreCatalogue(['Appricot : 20.4',
+    'Fridge : 1500',
+    'TV : 1499',
+    'Deodorant : 10',
+    'Boiler : 300',
+    'Apple : 1.25',
+    'Anti-Bug Spray : 15',
+    'T-Shirt : 10']
+)
