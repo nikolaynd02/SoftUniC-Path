f56d512e17a6d02a3677ca573cf6fd5862b782b4
Nikolay Dobrev <nickndobrev@gmail.com>
2022-09-28 23:09:05 +0300

    finished

--- JS Advanced/Object and Composition/Exercise.js
+++ JS Advanced/Object and Composition/Exercise.js
@@ -123,12 +123,31 @@ function StoreCatalogue(input){
     }
 }
 
-StoreCatalogue(['Appricot : 20.4',
-    'Fridge : 1500',
-    'TV : 1499',
-    'Deodorant : 10',
-    'Boiler : 300',
-    'Apple : 1.25',
-    'Anti-Bug Spray : 15',
-    'T-Shirt : 10']
-)
+//P.07
+function TownsToJSON(arr) {
+    let result = [];
+    const properties = arr[0].split(/\s*\|\s*/mg).filter(e => e.trim());
+
+    for (let i = 1; i < arr.length; i++) {
+        let data = arr[i].split(/\s*\|\s*/mg).filter(e => e.trim());
+
+        let name = data[0];
+        let latitude = Number(data[1]).toFixed(2);
+        let longtitude = Number(data[2]).toFixed(2);
+
+        let town = {};
+        town[properties[0]] = name;
+        town[properties[1]] = Number(latitude);
+        town[properties[2]] = Number(longtitude);
+
+        result.push(town);
+    }
+
+    let jsonString = JSON.stringify(result);
+    return jsonString;
+}
+
+console.log(TownsToJSON(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |']
+));
