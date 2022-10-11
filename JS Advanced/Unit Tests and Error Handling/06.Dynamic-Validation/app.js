2d0000880bec05c79c1c34f388124683adb9eee1
Nikolay Dobrev <nickndobrev@gmail.com>
2022-10-11 23:39:33 +0300

    ex 5 & 6

--- JS Advanced/Unit Tests and Error Handling/06.Dynamic-Validation/app.js
+++ JS Advanced/Unit Tests and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let emailElement = document.getElementById('email');
+    const pattern = /^([a-z]+@{1}[a-z]+\.{1}[a-z]+)$/;
+    emailElement.addEventListener('change', (e) => {
+        console.log(e.target);
+        if(pattern.test(e.target.value)){
+            e.currentTarget.removeAttribute('class');
+        }
+        else{
+            e.currentTarget.classList.add('error');
+        }
+    })
 }
